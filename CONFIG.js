8ba8149c488b623975a6d796c61f233326c101a6
himanshuverma5053-dot <himanshu.verma5053@gmail.com>
2026-07-23 12:17:47 +0530

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,12 @@
+
+// CONFIGURATION
+
+export const CONFIG = {
+    region: 'ap-south-1',
+    cognito: {
+        userPoolId: 'us-east-1_VKZn9Dc4v',
+        appClientId: '26ibua8ee0mvj0sm42c0qr3tou'
+    },
+    api: {
+        baseUrl: 'https://api.magadhtyres.com/prod'
+};
