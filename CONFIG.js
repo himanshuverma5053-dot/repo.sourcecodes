891d8d1d0446cc9be96030900af35a0c0fdd6564
himanshuverma5053-dot <himanshu.verma5053@gmail.com>
2026-07-23 12:38:29 +0530

    robin
    Updated Cognito userPoolId and appClientId in CONFIG.

--- CONFIG.js
+++ CONFIG.js
@@ -4,8 +4,8 @@
 export const CONFIG = {
     region: 'ap-south-1',
     cognito: {
-        userPoolId: 'us-east-1_VKZn9Dc4v',
-        appClientId: '26ibua8ee0mvj0sm42c0qr3tou'
+        userPoolId: 'us-east-1_FSZpv6nvK',
+        appClientId: '6i0mpg2bh7bl11r7fqhjhl55dl'
     },
     api: {
         baseUrl: 'https://api.magadhtyres.com/prod'
